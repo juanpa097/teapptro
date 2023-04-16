eba2c8f07650defe759d92371089f34a191dda32
Juan Penaloza <juan.pa097@gmail.com>
2023-04-16 10:00:12 +0100

    Added addAsFavorite to repository interface

--- lib/features/search/domain/repositories/events_repository.dart
+++ lib/features/search/domain/repositories/events_repository.dart
@@ -1,7 +1,10 @@
 import 'package:dartz/dartz.dart';
+
 import '../entities/event.dart';
 import '../entities/event_failure.dart';
 
 abstract class EventsRepository {
   Stream<Either<EventFailure, List<Event>>> watchAll();
+
+  Stream<Either<EventFailure, bool>> addAsFavorite(String eventId);
 }
